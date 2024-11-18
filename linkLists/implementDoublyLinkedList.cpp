8753948b58c16cffc1e64b54bb8f22f5bc38e6f4
Niraj singh <fgniraj@gmail.com>
2024-11-18 21:49:23 +0530

    Updated submodule LinkedList

---
+++ linkLists/implementDoublyLinkedList.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+        data = val;
+        next = prev = nullptr;
+    }
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+    }
+
+    // Insert at the beginning
+    void insertAtBeginning(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+
+    // Insert at the end
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
+    // Display the linked list
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " <-> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList list;
+    list.insertAtBeginning(10);
+    list.insertAtBeginning(20);
+    list.insertAtEnd(30);
+    list.display(); // Output: 20 <-> 10 <-> 30 <-> NULL
+
+    return 0;
+}
---
+++ linkLists/implementSinglyLinkedList.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    // Insert at the beginning
+    void insertAtBeginning(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Insert at the end
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    // Display the linked list
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.insertAtBeginning(10);
+    list.insertAtBeginning(20);
+    list.insertAtEnd(30);
+    list.display(); // Output: 20 -> 10 -> 30 -> NULL
+
+    return 0;
+}
