21bd6b571daf591094833ea33f29a096a486362e
Niraj singh <fgniraj@gmail.com>
2024-11-20 07:02:10 +0530

    deteect a cycle added

---
+++ linkLists/detectCycleInLinkedList.cpp
@@ -0,0 +1,13 @@
+bool hasCycle() {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+    return false;
+}
